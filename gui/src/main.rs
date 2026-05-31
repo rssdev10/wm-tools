c221d092b3cb55d9ac7a99b30c4fc8d5043fc077
Roman S Samarev <rssdev10@gmail.com>
2026-05-31 20:04:34 +0300

    cicd fix. And embedded an icon

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -7,14 +7,25 @@ mod serial;
 mod settings;
 
 use app::App;
+use iced::window;
 
 fn main() -> iced::Result {
     logging::init();
     log::info!("starting dso3d12-gui");
 
+    let icon = window::icon::from_file_data(
+        include_bytes!("../img/dso3d12_ico.png"),
+        None,
+    )
+    .ok();
+
     iced::application(App::new, App::update, App::view)
         .title(App::title)
         .theme(App::theme)
         .subscription(App::subscription)
+        .window(window::Settings {
+            icon,
+            ..Default::default()
+        })
         .run()
 }
